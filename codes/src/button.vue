24a852bf2db297e83a77242e20df74f37866c560
bowen-wu <13793248162@163.com>
2018-07-10 21:20:57 +0800

    feat(): add parcel and use single file components

---
+++ codes/src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        line-height: var(--line-height);
+        color: var(--color);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background-color: var(--button-bg);
+        &:hover{
+        border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
--- notes/parcel.md
+++ notes/parcel.md
@@ -1,2 +1,12 @@
 ## Parcel
 
+1. 安装： ` npm i -D parcel-bundler `
+2. Vue 使用 parcel： 在 package.json 中添加：
+    ```
+    {
+    // ...
+    "alias": {
+        "vue" : "./node_modules/vue/dist/vue.common.js"
+    }
+    }
+    ```
