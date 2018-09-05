5cc55c9e46d5c38d510a51ae4f7600b902cec925
bowen-wu <13793248162@163.com>
2018-09-06 00:05:27 +0800

    fix(demo): use Tabs component

--- src/demo.vue
+++ src/demo.vue
@@ -1,18 +1,46 @@
 <template>
     <div class="demo">
-        <g-button>hello world</g-button>
-        啦啦la
+        <g-tabs :selected.sync="selected" class="tabs" direction="horizontal">
+            <g-tabs-head>
+                <g-tabs-item name="women">
+                    <g-icon name="setting" class="icon"></g-icon>美女</g-tabs-item>
+                <g-tabs-item name="finance" disabled>财经</g-tabs-item>
+                <g-tabs-item name="sport">体育</g-tabs-item>
+                <template slot="actions">
+                    <g-button>click</g-button>
+                </template>
+            </g-tabs-head>
+            <g-tabs-body>
+                <g-tabs-pane name="women">美女相关资讯</g-tabs-pane>
+                <g-tabs-pane name="finance">财经相关资讯</g-tabs-pane>
+                <g-tabs-pane name="sport">体育相关资讯</g-tabs-pane>
+            </g-tabs-body>
+        </g-tabs>
     </div>
 </template>
 <script>
 import Button from './button';
+import Icon from './icon';
+import Tabs from './tabs/tabs.vue';
+import TabsHead from './tabs/tabs-head';
+import TabsBody from './tabs/tabs-body';
+import TabsItem from './tabs/tabs-item';
+import TabsPane from './tabs/tabs-pane';
 export default {
     name: 'bowen-demo',
     components: {
-        'g-button': Button
+        'g-button': Button,
+        'g-icon': Icon,
+        'g-tabs': Tabs,
+        'g-tabs-head': TabsHead,
+        'g-tabs-body': TabsBody,
+        'g-tabs-item': TabsItem,
+        'g-tabs-pane': TabsPane
     },
-    created() {
-        console.log(Button);
+    data() {
+        return {
+            selected: 'sport'
+        };
     }
 };
 </script>
