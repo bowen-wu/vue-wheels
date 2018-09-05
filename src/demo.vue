0015168095dd11d581b1f064af79a1e0dc1a62b1
bowen-wu <13793248162@163.com>
2018-09-05 23:50:12 +0800

    feat(building tool): building tool change to vue-cli@3.0.1 because parcel very very slow

---
+++ src/demo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="demo">
+        <g-button>hello world</g-button>
+        啦啦la
+    </div>
+</template>
+<script>
+import Button from './button';
+export default {
+    name: 'bowen-demo',
+    components: {
+        'g-button': Button
+    },
+    created() {
+        console.log(Button);
+    }
+};
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:root {
+    --font-size: 14px;
+    --line-height: 32px;
+    --color: #333;
+    --button-bg: #fff;
+    --button-active-bg: #eee;
+    --border-radius: 4px;
+    --border-color: #999;
+    --border-color-hover: #666;
+}
+
+body {
+    font-size: var(--font-size);
+}
+</style>
+
